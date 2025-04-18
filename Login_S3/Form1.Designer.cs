2b1a5c001870f3bf582908296cb346d2cbe29bda
Veronica <vgalbicka@gmail.com>
2025-04-18 05:54:49 -0300

    camufla contraseña

--- Login_S3/Form1.Designer.cs
+++ Login_S3/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Login_S3
 {
-    partial class Form1
+    partial class frmPrincipal
     {
         /// <summary>
         /// Variable del diseñador necesaria.
@@ -28,27 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
             this.lblTitulo = new System.Windows.Forms.Label();
+            this.picBoxUsuario = new System.Windows.Forms.PictureBox();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxUsuario)).BeginInit();
             this.SuspendLayout();
             // 
             // lblTitulo
             // 
             this.lblTitulo.AutoSize = true;
-            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitulo.Location = new System.Drawing.Point(279, 41);
+            this.lblTitulo.Font = new System.Drawing.Font("Roboto", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(200, 37);
             this.lblTitulo.Name = "lblTitulo";
-            this.lblTitulo.Size = new System.Drawing.Size(153, 25);
+            this.lblTitulo.Size = new System.Drawing.Size(216, 34);
             this.lblTitulo.TabIndex = 0;
             this.lblTitulo.Text = "Diseño de Login";
             // 
-            // Form1
+            // picBoxUsuario
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.picBoxUsuario.Image = ((System.Drawing.Image)(resources.GetObject("picBoxUsuario.Image")));
+            this.picBoxUsuario.Location = new System.Drawing.Point(61, 103);
+            this.picBoxUsuario.Name = "picBoxUsuario";
+            this.picBoxUsuario.Size = new System.Drawing.Size(147, 156);
+            this.picBoxUsuario.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.picBoxUsuario.TabIndex = 1;
+            this.picBoxUsuario.TabStop = false;
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.Font = new System.Drawing.Font("Roboto", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnIngresar.Location = new System.Drawing.Point(265, 203);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(152, 55);
+            this.btnIngresar.TabIndex = 2;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = true;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Font = new System.Drawing.Font("Roboto", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUsuario.ForeColor = System.Drawing.Color.Black;
+            this.txtUsuario.Location = new System.Drawing.Point(248, 112);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(193, 23);
+            this.txtUsuario.TabIndex = 3;
+            this.txtUsuario.Text = "Usuario";
+            this.txtUsuario.Enter += new System.EventHandler(this.txtUsuario_Enter);
+            this.txtUsuario.Leave += new System.EventHandler(this.txtUsuario_Leave);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Roboto", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.ForeColor = System.Drawing.Color.Black;
+            this.txtPassword.Location = new System.Drawing.Point(248, 161);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(193, 23);
+            this.txtPassword.TabIndex = 4;
+            this.txtPassword.Text = "Contraseña";
+            this.txtPassword.Enter += new System.EventHandler(this.txtPassword_Enter);
+            this.txtPassword.Leave += new System.EventHandler(this.txtPassword_Leave);
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(558, 348);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.btnIngresar);
+            this.Controls.Add(this.picBoxUsuario);
             this.Controls.Add(this.lblTitulo);
-            this.Name = "Form1";
-            this.Text = "Form1";
+            this.Font = new System.Drawing.Font("Roboto", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.picBoxUsuario)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -57,6 +114,10 @@
         #endregion
 
         private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.PictureBox picBoxUsuario;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtPassword;
     }
 }
 
--- Login_S3/Form1.cs
+++ Login_S3/Form1.cs
@@ -10,11 +10,52 @@ using System.Windows.Forms;
 
 namespace Login_S3
 {
-    public partial class Form1 : Form
+    public partial class frmPrincipal : Form
     {
-        public Form1()
+        public frmPrincipal()
         {
             InitializeComponent();
         }
+
+        private void txtUsuario_Enter(object sender, EventArgs e)
+        {
+            //este evento se ejecuta cuando llega el foco
+            if (txtUsuario.Text=="Usuario")
+            {
+                txtUsuario.Text = "";
+
+            }
+        }
+
+        private void txtUsuario_Leave(object sender, EventArgs e)
+        {
+            //este evento se ejecuta cuando se va el foco sin haber ingresado contenido, (vuelve a dar la guia)
+            if (txtUsuario.Text == "")
+            {
+                txtUsuario.Text = "Usuario";
+            }
+        }
+
+        private void txtPassword_Enter(object sender, EventArgs e)
+        {
+            //este evento borra la palabra guia y camufla el password
+            if (txtPassword.Text == "Contraseña")
+            {
+                txtPassword.Text = "";
+                txtPassword.UseSystemPasswordChar = true;
+            }
+            
+        }
+
+        private void txtPassword_Leave(object sender, EventArgs e)
+        {
+            //este evento se ejecuta cuando se va el foco sin haber ingresado contenido, (vuelve a dar la guia)
+            if (txtPassword.Text == "")
+            {
+                txtPassword.Text = "Contraseña";
+                txtPassword.UseSystemPasswordChar = false;  //saca el camuflado si se va el foco sin ingreso
+            }
+        }
     }
 }
+
